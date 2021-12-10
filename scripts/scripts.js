11053e7c4b41a3046e5ea89056f9dabdfa51887a
James Wu <wu.james@hotmail.com>
2021-12-10 02:39:02 -0500

    heading

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+// typing animaiton
+var typed = new Typed('#role', {
+    strings: ['Software Developer','Computer Scientist'],
+    typeSpeed:100,
+    backDelay:3000,
+    showCursor:false,
+    loop:true
+});
