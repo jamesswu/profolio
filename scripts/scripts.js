036dd0b5c614125f943a6323dd10fd9a0d409f67
James Wu <wu.james@hotmail.com>
2022-01-03 01:14:08 -0500

    typing msg

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 // typing animaiton
 var typed = new Typed('#role', {
-    strings: ['Front-end Web Developer','Software Developer','Computer Scientist'],
+    strings: ['Front-end Developer','Software Developer','Computer Scientist'],
     typeSpeed:100,
     backDelay:3000,
     showCursor:false,
